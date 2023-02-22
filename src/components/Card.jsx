be22ece90e223d2341c6d060b6070640313225f8
Persona743 <siyavashshahidi7@gmail.com>
2023-02-22 18:34:35 +0330

    card added

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,5 +1,21 @@
 import React from 'react';
+import card1 from '../assets/images/swimmer.png';
+import starImage from '../assets/images/Star 1.png';
 
 export default function Card() {
-    return <div>Card</div>;
+    return (
+        <div className="card">
+            <img src={card1} className="card--image" />
+            <div className="card--context">
+                <img src={starImage} className="card--star" />
+                <span>5.0 </span>
+                <span className="gray">(6) . </span>
+                <span className="gray">USA</span>
+            </div>
+            <p>Life lessons with Katie Zaferes</p>
+            <p>
+                <span className="bold">From $136</span> / person
+            </p>
+        </div>
+    );
 }
