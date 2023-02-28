3c3a1569ca1ddd7d4624dd69e91ed3c5afb9548f
Persona743 <siyavashshahidi7@gmail.com>
2023-02-28 21:26:47 +0330

    data-task added

--- src/components/pages/task/NavbarTask.jsx
+++ src/components/pages/task/NavbarTask.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default function Navbar() {
     return (
         <nav className="task--nav">
             <img src="/assets/images/Fill 213.png" className="task--logo" />
             <h5 className="task--title">my travel journal.</h5>
+            <Link to="/" className="task--btn">
+                Home
+            </Link>
         </nav>
     );
 }
--- src/components/pages/task/Task.jsx
+++ src/components/pages/task/Task.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import NavbarTask from './NavbarTask';
+import CardTask from './CardTask';
 
 export default function Task() {
     return (
         <div>
             <NavbarTask />
+            <CardTask />
         </div>
     );
 }
